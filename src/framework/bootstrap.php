495f23cfbcf5143b9f42733f9609fa77a2ec5eff
sven <121642038@qq.com>
2017-12-09 12:57:06 +0800

    update composer.json

--- src/framework/bootstrap.php
+++ src/framework/bootstrap.php
@@ -6,3 +6,4 @@ define('FRAMEWORK_ROOT_PATH', realpath(dirname(__FILE__)) . DS);
 require_once FRAMEWORK_ROOT_PATH . '/function.php';
 require_once FRAMEWORK_ROOT_PATH . '/FilterSqlInject.php';
 require_once FRAMEWORK_ROOT_PATH . '/ErrorHandler.php';
+require_once FRAMEWORK_ROOT_PATH . '/HornetEngine.php';
